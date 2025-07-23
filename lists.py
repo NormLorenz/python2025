1d91af8466ebe7e9352d93cecb289230251a461c
Norm Lorenz <normlorenz@gmail.com>
2025-07-23 08:54:49 -0700

    Add new file.

---
+++ lists.py
@@ -0,0 +1,16 @@
+people: list[str] = [
+    "Alice",
+    "Bob",
+    "Charlie"
+]
+
+print (people)
+
+people.append("Diana")
+print(people)   
+
+people.insert(0, "Eve")
+print(people)
+
+people.append("Frank")
+print(people)
